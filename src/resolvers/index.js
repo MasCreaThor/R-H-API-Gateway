701ec6ba8189eb3bfd215f357e2e983e50b1b473
Yadir <yamunozr22@itp.edu.co>
2025-04-25 01:23:10 -0500

    Diseño de esquema GraphQL para API Gateway

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,49 @@
 // Resolvers para GraphQL
 const resolvers = {
     Query: {
-      _: () => true
+      _: () => true,
+      // Resolvers para auth
+      me: () => null,
+      validateToken: () => false,
+      
+      // Resolvers para people
+      getPeopleInfo: () => null,
+      
+      // Resolvers para hoteles
+      getHoteles: () => [],
+      getHotelById: () => null,
+      getHotelesDestacados: () => [],
+      
+      // Resolvers para habitaciones
+      getRoomsByHotelId: () => [],
+      getRoomById: () => null,
+      checkRoomAvailability: () => false,
+      
+      // Resolvers para reservas
+      getUserBookings: () => [],
+      getBookingById: () => null,
+      getHotelBookings: () => []
     },
     Mutation: {
-      _: () => true
+      _: () => true,
+      // Resolvers para auth
+      register: () => null,
+      login: () => null,
+      refreshToken: () => null,
+      
+      // Resolvers para people
+      updatePeopleInfo: () => null,
+      
+      // Resolvers para habitaciones
+      createRoom: () => null,
+      updateRoom: () => null,
+      deleteRoom: () => false,
+      updateRoomAvailability: () => null,
+      
+      // Resolvers para reservas
+      createBooking: () => null,
+      updateBookingStatus: () => null,
+      cancelBooking: () => null
     }
   };
   
--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,16 +1,29 @@
 import { gql } from 'apollo-server-express';
+import authTypes from './authTypes.js';
+import peopleTypes from './peopleTypes.js';
+import hotelTypes from './hotelTypes.js';
+import roomTypes from './roomTypes.js';
+import bookingTypes from './bookingTypes.js';
 
-// Definición de tipos GraphQL
-const typeDefs = gql`
-  # Tipo Query principal
+// Definición de tipos base
+const baseTypeDefs = gql`
   type Query {
     _: Boolean
   }
 
-  # Tipo Mutation principal
   type Mutation {
     _: Boolean
   }
 `;
+
+// Combinar todos los tipos
+const typeDefs = [
+  baseTypeDefs,
+  authTypes,
+  peopleTypes,
+  hotelTypes,
+  roomTypes,
+  bookingTypes
+];
 
 export default typeDefs;
